059a1bf75288bbebbd4cab36110004c3e2fd6668
EldiShaba <101250259+EldiShaba@users.noreply.github.com>
2023-04-26 20:02:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int numero1 = 0,numero2 = 1,prossimo_numero = 0, n;
+    cin >> n;
+    cout << "Serie di fibonacci " << numero1 << ", " <<numero2 << ", ";
+    prossimo_numero = numero1 + numero2;
+
+    while(prossimo_numero <= n) {
+        cout << prossimo_numero << ", ";
+        numero1 = numero2;
+        numero2 = prossimo_numero;
+        prossimo_numero = numero1 + numero2;
+         }
+    return 0;
 }
