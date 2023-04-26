b6b49bb5219dcbb144a065ff1fbce7bf864383c2
EldiShaba <101250259+EldiShaba@users.noreply.github.com>
2023-04-26 21:40:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,18 +2,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n1=0,n2=1,n3,i,number;
+    int n1=0,n2=1,n3,i,numero;
     cout<<"numeri di elementi ";
-    cin>>number;
-    if(number<=2){
+    cin>>numero;
+    if(numero>2){
+        cout<<n1<<endl;
+        cout<<n2<<endl;
+        for(i=2;i<numero;++i){
+            n3=n1+n2;
+            cout<<n3<<endl;
+            n1=n2;
+            n2=n3;
+    }}
+    else
         cout<<"errore";
-    }
-    else cout<<n1<<" "<<n2<<" ";
-    
-    for(i=2;i<number;++i){
-        n3=n1+n2;
-        cout<<n3<<" ";
-        n1=n2;
-        n2=n3;
-    }
     return 0;
+}
